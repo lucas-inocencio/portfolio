4a58ff36644f4e924c9f0b075b9e356ac4ee5065
Lucas <li,lucasinocencio@gmail.com>
2025-02-06 13:08:32 -0300

    first commit

--- script.js
+++ script.js
@@ -1,43 +1,5 @@
-const elemText = document.querySelector(".about");
-const elemBotaoLight = document.querySelector(".botao-light");
-const elemBotaoDark = document.querySelector(".botao-dark");
-const elemInformations = document.querySelector(".informations");
-const elemBottonInformations = document.querySelector(".botton-informations");
+const elemBottonLightDark = document.querySelector(".botton-light-dark");
 
-function alterarClasse() {
-  console.log("Você clicou no texto Sobre mim");
-  alert("Você clicou no texto Sobre mim");
-
-  elemText.classList.toggle("red");
-  elemText.classList.toggle("yellow");
-
-  elemText.textContent = "Sobre mim (clicado)";
-}
-
-// Eventos
-elemText.addEventListener("click", alterarClasse);
-
-// Adicionar o evento de clique no botão do rodapé
-elemInformations.addEventListener("click", function () {
-  console.log("Clicou no botão de informações");
-  elemInformations.classList.toggle("visivel");
-
-  console.log(elemInformations.classList.contains("visivel"));
-
-  if (elemInformations.classList.contains("visivel")) {
-    elemInformations.textContent = "Ocultar informações";
-  } else {
-    elemInformations.textContent = "Exibir informações";
-  }
-});
-
-// Adicionar evento de clique nos botões Light/Dark
-elemBotaoLight.addEventListener("click", function () {
-  console.log("Clicou no botão Light");
-  document.body.classList.add("light");
-});
-
-elemBotaoDark.addEventListener("click", function () {
-  console.log("Clicou no botão Dark");
-  document.body.classList.remove("light");
+elemBottonLightDark.addEventListener("click", function () {
+  document.body.classList.toggle("light");
 });
