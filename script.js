804942c99c160c68a148910039af06437870c4b6
Lucas <li,lucasinocencio@gmail.com>
2025-02-22 10:17:22 -0300

    second commit

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 const elemBottonLightDark = document.querySelector(".botton-light-dark");
+const projects = document.getElementById("projects");
 
-elemBottonLightDark.addEventListener("click", function () {
-  document.body.classList.toggle("light");
+elemBottonLightDark.addEventListener("click", () => document.body.classList.toggle("light"));
+
+const imgPaths = [
+  "algorithms-and-programming",
+  "computational-linear-algebra",
+  "database-systems",
+  "graph-theory",
+  "high-performance-computing",
+  "information-security",
+  "knowledge-management",
+  "machine-learning",
+  "portfolio",
+  "software-engineering",
+  "software-quality",
+  "spotify",
+  "spotify-vanilla"
+];
+
+imgPaths.forEach((imgPath) => {
+  const imgName = imgPath.replaceAll("-", " ");
+  const project = document.createElement("div");
+  project.classList.add("project");
+  project.innerHTML = `
+    <h3>${imgName}</h3>
+    <img width="600" height="400" src="./img/${imgPath}.png" alt="Projeto ${imgName}" />
+    <a target="_blank" href="https://github.com/lucas-inocencio/${imgPath}">Clique aqui para acessar</a>
+  `;
+  console.log(imgPath);
+  projects.appendChild(project);
 });
+
