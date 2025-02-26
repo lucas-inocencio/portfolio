26b2317646392a57efc5f37758a0bee6c461e887
Lucas <li,lucasinocencio@gmail.com>
2025-02-26 16:37:13 -0300

    third commit

--- script.js
+++ script.js
@@ -6,29 +6,35 @@ elemBottonLightDark.addEventListener("click", () => document.body.classList.togg
 const imgPaths = [
   "algorithms-and-programming",
   "computational-linear-algebra",
+  "computer-architecture",
+  "computer-networks",
+  "data-structures",
   "database-systems",
+  "digital-systems",
   "graph-theory",
   "high-performance-computing",
   "information-security",
   "knowledge-management",
+  "logic-circuits",
   "machine-learning",
   "portfolio",
+  "probability-and-statistics",
   "software-engineering",
   "software-quality",
   "spotify",
-  "spotify-vanilla"
+  "spotify-vanilla",
+  "telecommunications",
 ];
 
 imgPaths.forEach((imgPath) => {
   const imgName = imgPath.replaceAll("-", " ");
   const project = document.createElement("div");
   project.classList.add("project");
+  const isPortuguese = document.body.classList.contains("portuguese");
   project.innerHTML = `
     <h3>${imgName}</h3>
-    <img width="600" height="400" src="./img/${imgPath}.png" alt="Projeto ${imgName}" />
-    <a target="_blank" href="https://github.com/lucas-inocencio/${imgPath}">Clique aqui para acessar</a>
+    <img width="600" height="400" src="${isPortuguese ? './img/' : '../img/'}${imgPath}.png" alt="${isPortuguese ? 'Projeto ' : ''}${imgName} ${isPortuguese ? '' : 'Project'}" />
+    <a target="_blank" href="https://github.com/lucas-inocencio/${imgPath}">${isPortuguese ? 'Clique aqui para acessar' : 'Click here to access'}</a>
   `;
-  console.log(imgPath);
   projects.appendChild(project);
 });
-
